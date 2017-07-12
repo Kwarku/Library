6fe3cd1cebfeef95751f9020eeb010a122e506ea
Kwarku <p.kuklicz@gmail.com>
2017-07-12 16:47:13 +0200

    implementacja rekurencji

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -4,10 +4,10 @@ package app;
  * Created by Pawel on 25.05.2017.
  */
 public class LibraryApp {
-    public static final String APP_NAME = "Biblioteka v0.92";
     public static void main(String[] args) {
-        System.out.println(APP_NAME);
+        final String appName = "Biblioteka v0.92";
+        System.out.println(appName);
         LibraryControl libraryControl = new LibraryControl();
-        libraryControl.controlLoop();
+        libraryControl.controlLoop(null);
     }
 }
