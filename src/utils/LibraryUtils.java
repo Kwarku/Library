6e592170c991e6a094d648d21b89061734aabca3
Kwarku <p.kuklicz@gmail.com>
2017-07-20 14:42:37 +0200

    wprowadzenie strumieni
    zastapienie starej powtarzajacej sie metody wyswietlania nowa uniwersalna dla obu clas zmniejszajac ilosc kodu i zwiekszajac czytelnosc
    Wszystko dzieki obsludze strumieni

--- src/utils/LibraryUtils.java
+++ src/utils/LibraryUtils.java
@@ -9,53 +9,31 @@ import java.util.*;
 
 public class LibraryUtils {
 
+    public static void printBooks(Library   lib){
+        printPublications(lib,  Book.class);
+    }
 
-    public static void printBooks(Library lib) {
-        List<Publication> publications = new ArrayList<>();
-        publications.addAll(lib.getPublications().values());
-        Collections.sort(publications, new Library.AlphabeticalComparaotr());
+    public static void printMagazines(Library   lib){
+        printPublications(lib,  Magazine.class);
+    }
 
-        int countBooks = 0;
-       for (Publication p: publications){
-           if (p instanceof Book){
-               System.out.println(p);
-               countBooks++;
-           }
-       }
-        if(countBooks == 0) {
-            System.out.println("Brak książek w bibliotece");
+    private static void printPublications(Library   lib,    Class   cl){
+        long    countMagazines  =   lib.getPublications().values().stream()     //zwraca strumien utworzony z zbioru
+                .filter(cl::isInstance)     //filtruje po klasie wywolujacej
+                .sorted(new Library.AlphabeticalComparaotr())   //sortuje alfabetycznie zgodnie z metoda sortujaca
+                .peek(System.out::println)      //wyswietla
+                .count();
+
+        if (countMagazines  ==  0){
+            System.out.println("w bibliotece nie znaleziono publikacji typu "   +   cl.getSimpleName());
         }
     }
 
-    public static void printMagazines(Library lib) {
-        List<Publication> publications = new ArrayList<>();
-        publications.addAll(lib.getPublications().values());
-
-        Collections.sort(publications, new Library.AlphabeticalComparaotr());
-        int countMagazines = 0;
-        for (Publication p: publications){
-            if (p instanceof Magazine){
-                System.out.println(p);
-                countMagazines++;
-            }
-        }
-        if(countMagazines == 0) {
-            System.out.println("Brak magazynów w bibliotece");
-        }
+    public static void printUsers(Library   lib){
+        lib.getUsers().values().stream()
+                .sorted((a, b)   ->  a.getLastName().compareTo(b.getLastName()))
+                .forEach(System.out :: println);
     }
 
-    //dodanie metody wyswietlajaca liste uzytkownikow
-    public static void printUsers(Library lib){
-        List<LibraryUser> users = new ArrayList<>();
-        users.addAll(lib.getUsers().values());
-        Collections.sort(users, new Comparator<LibraryUser>() {
-            @Override
-            public int compare(LibraryUser o1, LibraryUser o2) {
-                return o1.getLastName().compareTo(o2.getLastName());
-            }
-        });
-        for (LibraryUser u : users){
-            System.out.println(u);
-        }
-    }
+
 }
